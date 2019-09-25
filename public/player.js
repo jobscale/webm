0fa3cfef86cfc208b711dcb3b53d890b11d476c4
Your Name <you@example.com>
2019-09-25 13:48:39 +0900

    digest auth fetch

--- public/player.js
+++ public/player.js
@@ -3,7 +3,7 @@ class Player {
   constructor(video) {
     this.video = video || document.querySelector('video');
   }
-  play(assetURL, mimeCodec) {
+  play(assetURL, mimeCodec, headers) {
     // const mimeCodec = 'video/webm; codecs="vp8,vorbis"';
     this.assetURL = assetURL || 'frag_bunny.mp4';
     this.mimeCodec = mimeCodec || 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
@@ -11,15 +11,18 @@ class Player {
       this.mediaSource = new MediaSource;
       // logger.log(mediaSource.readyState); // closed
       this.video.src = URL.createObjectURL(this.mediaSource);
-      this.mediaSource.addEventListener('sourceopen', () => this.sourceOpen());
+      this.mediaSource.addEventListener('sourceopen', () => this.sourceOpen(headers));
     } else {
       logger.error('Unsupported MIME type or codec: ', this.mimeCodec);
     }
   }
-  sourceOpen() {
+  sourceOpen(headers) {
     // logger.log(this.readyState); // open
     const sourceBuffer = this.mediaSource.addSourceBuffer(this.mimeCodec);
-    this.fetchAB(this.assetURL, buf => {
+    this.fetchAB({
+      url: this.assetURL,
+      headers,
+    }, buf => {
       sourceBuffer.addEventListener('updateend', () => {
         this.mediaSource.endOfStream();
         this.video.play().catch(logger.error);
@@ -28,10 +31,13 @@ class Player {
       sourceBuffer.appendBuffer(buf);
     });
   }
-  fetchAB (url, cb) {
-    logger.log(url);
+  fetchAB (options, cb) {
+    logger.log(options);
     const xhr = new XMLHttpRequest;
-    xhr.open('get', url);
+    xhr.open('get', options.url);
+    Object.keys(options.headers).forEach(key => {
+      xhr.setRequestHeader(key, options.headers[key]);
+    });
     xhr.responseType = 'arraybuffer';
     xhr.onload = () => {
       cb(xhr.response);

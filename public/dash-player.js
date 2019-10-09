2422c7848e380f00e8b70e2bfeec431bc1002b58
jobscale <jobscale@users.noreply.github.com>
2019-10-09 10:10:42 +0000

    without XMLHttpRequest

--- public/dash-player.js
+++ public/dash-player.js
@@ -1,29 +1,69 @@
+const env = {
+  logging: true,
+  version: '0.1.0',
+};
 const logger = console;
 logger.json = (...argv) => {
   if (argv.length === 1) return logger.info(JSON.stringify(argv[0], null, 2));
   logger.info(JSON.stringify({ argv }, null, 2));
 };
+logger.info(`Version: ${env.version}`);
+if (!env.logging) {
+  const native = () => {};
+  logger.log = native;
+  logger.info = native;
+  logger.warn = native;
+  logger.error = native;
+  logger.json = native;
+}
 
 class Player {
   constructor(video) {
     this.video = video || document.querySelector('video');
-    this.fastLength = 5 * 1024 * 1024;
-    this.totalSegments = 0;
-    this.segmentLength = 0;
-    this.segmentDuration = 5;
-    this.bytesFetched = 0;
-    this.requestedSegments = [];
   }
-  play(assetURL, mimeCodec, headers) {
-    this.assetURL = assetURL || 'frag_bunny.mp4';
-    this.mimeCodec = mimeCodec || 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
+  initialize() {
+    const options = {
+      headers: this.headers,
+    };
+    return fetch(this.assetURL, options)
+    .then(res => res.text())
+    .then(mpd => this.parseMPD(mpd))
+    .catch(e => logger.error(e.message));
+  }
+  parseMPD(mpd) {
+    this.infoRange = /Initialization range.+(\d+-\d+)/.exec(mpd)[1];
+    const basePath = this.assetURL.indexOf('/') !== -1 ? /(.*\/)/.exec(this.assetURL)[1] : '';
+    this.baseURL = `${basePath}${/BaseURL>(.+)<\/BaseURL/.exec(mpd)[1]}`;
+    this.segmentDuration = /duration="(\d+)"/.exec(mpd)[1];
+    this.segments = mpd.match(/mediaRange="\d+-\d+"/g)
+    .map(v => ({ range: /.+"(\d+-\d+)"/.exec(v)[1] }));
+    this.mimeType = /mimeType="([\w/]+)"/.exec(mpd)[1];
+    this.codecs = /(codecs="[\w\d\.,-]+")/.exec(mpd)[1];
+    logger.json({
+      infoRange: this.infoRange,
+      baseURL: this.baseURL,
+      segmentDuration: this.segmentDuration,
+      totalSegments: this.segments.length,
+      mimeType: this.mimeType,
+      codecs: this.codecs,
+    });
+    if (this.segmentDuration < 5000) this.segmentDuration = 5000;
+  }
+  play(assetURL, headers) {
+    this.assetURL = assetURL || 'frag_bunny_dash.mpd';
     this.headers = headers || {};
-    if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
+    this.queue = [];
+    this.initialize()
+    .then(() => this.start());
+  }
+  start() {
+    this.mimeCodec = `${this.mimeType}; ${this.codecs}` || 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
+    if ('MediaSource' in window && MediaSource.isTypeSupported(this.mimeCodec)) {
       this.mediaSource = new MediaSource();
       this.video.src = URL.createObjectURL(this.mediaSource);
       this.mediaSource.addEventListener('sourceopen', () => this.sourceOpen());
     } else {
-      logger.error('Unsupported MIME type or codec: ', mimeCodec);
+      logger.error('Unsupported MIME type or codec: ', this.mimeCodec);
     }
   }
   actionPlay() {
@@ -34,135 +74,140 @@ class Player {
     };
     const canPlay = () => {
       this.video.removeEventListener('canplay', canPlay);
-      this.totalSegments = Math.ceil(this.video.duration / this.segmentDuration);
-      this.segmentLength = Math.ceil(this.fileLength / this.totalSegments);
       logger.json({
-        segmentLength: this.segmentLength,
         fileLength: this.fileLength,
-        totalSegments: this.totalSegments,
         videoDuration: this.video.duration,
         segmentDuration: this.segmentDuration,
       });
       this.video.addEventListener('timeupdate', () => this.checkBuffer());
-      this.video.addEventListener('seeking', pos => this.seek(pos));
+      this.video.addEventListener('seeking', event => this.seek(event));
       document.querySelector('body').addEventListener('click', onPlay);
       // document.querySelector('body').click();
     };
     this.video.addEventListener('canplay', canPlay);
   }
   sourceOpen() {
+    if (this.mediaSource.sourceBuffers.length) {
+      logger.info('sourceOpen duplicate.');
+      return;
+    }
     this.sourceBuffer = this.mediaSource.addSourceBuffer(this.mimeCodec);
     this.getFileLength({
-      url: this.assetURL,
+      url: this.baseURL,
       headers: this.headers,
     }, fileLength => {
       logger.log((fileLength / 1024 / 1024).toFixed(2), 'MB');
       this.fileLength = fileLength;
-      this.fetchRange({
-        url: this.assetURL,
-        headers: this.headers,
-      }, 0, this.fastLength - 1, chank => this.appendSegment(chank));
       this.actionPlay();
+      this.getInfo();
     });
   }
-  getFileLength(options, cb) {
-    const xhr = new XMLHttpRequest;
-    xhr.open('head', options.url);
-    Object.keys(options.headers || {}).forEach(key => {
-      xhr.setRequestHeader(key, options.headers[key]);
-    });
-    xhr.onload = () => {
-      cb(xhr.getResponseHeader('content-length'));
-    };
-    xhr.send();
-  }
-  fetchRange(options, start, end, cb) {
-    console.json('time to fetch next chunk', {
-      currentTime: this.video.currentTime,
-      start,
-      end,
-      range: end - start + 1,
-    });
-    const xhr = new XMLHttpRequest;
-    xhr.open('get', options.url);
-    Object.keys(options.headers || {}).forEach(key => {
-      xhr.setRequestHeader(key, options.headers[key]);
-    });
-    xhr.responseType = 'arraybuffer';
-    xhr.setRequestHeader('Range', 'bytes=' + start + '-' + end);
-    xhr.onload = () => {
-      cb(xhr.response);
-    };
-    xhr.send();
-  }
-  inHead(pos) {
-    return pos < this.fastLength;
-  }
-  fetchSegment(segment, next) {
-    const finish = segment === this.totalSegments && this.haveAllSegments() && this.getCurrentSegment() === this.totalSegments - 1;
-    if (finish) {
-      logger.log('last segment', this.mediaSource.readyState);
-      this.mediaSource.endOfStream();
-      this.video.removeEventListener('timeupdate', () => this.checkBuffer());
-      return Promise.resolve({ status: 'finished.' });
-    }
-    if (!this.shouldFetchSegment(segment)) return Promise.resolve({ status: 'segment exist.' });
-    const promise = {};
-    promise.instance = new Promise((...argv) => {
-      [ promise.resolve, promise.reject ] = argv;
-    });
-    this.requestedSegments[segment] = true;
-    let start = this.segmentLength * segment;
-    let end = this.segmentLength * (segment + 1) - 1;
-    if (this.inHead(end)) return Promise.resolve({ status: 'segment exist.' });
-    if (this.inHead(start)) start = this.fastLength;
-    if (end >= this.fileLength) end = this.fileLength - 1;
+  getInfo() {
     this.fetchRange({
-      url: this.assetURL,
+      url: this.baseURL,
       headers: this.headers,
-    }, start, end, chank => {
+    }, this.infoRange, chank => {
       this.appendSegment(chank);
-      promise.resolve({ status: 'ok' });
+      this.checkBuffer();
     });
-    return promise.instance;
+  }
+  getFileLength(params, cb) {
+    const options = {
+      method: 'HEAD',
+      headers: params.headers,
+    };
+    fetch(params.url, options)
+    .then(res => res.headers.get('content-length'))
+    .then(length => cb(length));
+  }
+  fetchRange(params, range, cb) {
+    const options = {
+      method: 'GET',
+      headers: params.headers,
+    };
+    options.headers.Range = `bytes=${range}`,
+    fetch(params.url, options)
+    .then(res => res.arrayBuffer())
+    .then(chunk => cb(chunk));
+  }
+  fetchSegment(segment, params) {
+    if (this.interrupt || segment > params.last) return params.finish();
+    const next = () => {
+      this.fetchSegment(segment + 1, params);
+    };
+    if (this.segments[segment].loaded) return next();
+    console.json('time to fetch next chunk', {
+      current: `${segment + 1} / ${this.segments.length}`,
+      range: this.segments[segment].range,
+    });
+    this.segments[segment].loaded = true;
+    this.fetchRange({
+      url: this.baseURL,
+      headers: this.headers,
+    }, this.segments[segment].range, chank => {
+      this.appendSegment(chank, segment);
+      next();
+    });
   }
   checkBuffer() {
-    if (this.mediaSource.readyState === 'ended') return;
     const currentSegment = this.getCurrentSegment();
-    const ahead = async () => {
+    logger.log(`${currentSegment} - ${this.video.currentTime}`);
+    const ahead = () => {
       this.inWork = true;
-      const last = Math.min(currentSegment + 8, this.totalSegments + 1);
-      for (let i = 0; i < last; i++) {
-        await this.fetchSegment(i);
-      }
-      delete this.inWork;
+      const finish = () => {
+        delete this.inWork;
+        delete this.interrupt;
+        if (this.segments.length - 1 === currentSegment) this.lastSegment();
+      };
+      const last = Math.min(currentSegment + 8, this.segments.length - 1);
+      this.fetchSegment(currentSegment, { last, finish });
     };
     if (this.inWork) return;
+    if (this.interrupt) {
+      setTimeout(() => {
+        this.interrupt = false;
+        this.checkBuffer();
+      }, 8000);
+    }
     ahead();
   }
-  appendSegment(chunk) {
-    this.sourceBuffer.appendBuffer(chunk);
+  appendSegment(chunk, segment) {
+    if (chunk) this.queue.push({ chunk, segment });
+    if (this.sourceBuffer.updating) return setTimeout(() => this.appendSegment(), 20);
+    const queue = this.queue.shift();
+    logger.log(`append: ${queue.segment + 1}`);
+    this.sourceBuffer.appendBuffer(queue.chunk);
+  }
+  lastSegment() {
+    logger.log('last segment', this.mediaSource.readyState);
+    if (this.sourceBuffer.updating) return setTimeout(() => this.lastSegment(), 1000);
+    if (this.mediaSource.readyState === 'ended') return;
+    this.mediaSource.endOfStream();
+    this.clearCache();
+    this.segments[this.segments.length - 1].loaded = true;
+  }
+  clearCache() {
+    this.segments.forEach(segment => segment.loaded = false);
   }
   seek(event) {
     logger.json({
       readyState: this.mediaSource.readyState,
       event,
     });
+    this.interrupt = true;
+    const afterCheck = () => {
+      if (this.inWork) return setTimeout(() => afterCheck(), 200);
+      this.clearCache();
+      this.checkBuffer();
+    };
+    setTimeout(() => afterCheck(), 200);
     if (this.mediaSource.readyState !== 'open') {
       logger.log('seek but not open?');
-      return;
     }
   }
   getCurrentSegment() {
-    return Math.floor(this.video.currentTime / this.segmentDuration);
-  }
-  haveAllSegments() {
-    return this.requestedSegments.every(val => {
-      return !!val;
-    });
-  }
-  shouldFetchSegment(segment) {
-    if (segment >= this.totalSegments) return false;
-    return !this.requestedSegments[segment];
+    let current = (this.video.currentTime * 1000) - (this.segmentDuration * 0.5);
+    if (current < 0) current = 0;
+    return Math.floor(current / this.segmentDuration);
   }
 }
